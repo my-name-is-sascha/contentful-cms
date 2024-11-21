e24937dae598d68c4c4cb84a758eb1710c59747b
sascha <sascha.mauch@endava.com>
2024-11-21 17:14:12 +0100

    added projects component

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -1,6 +1,40 @@
-import './fetchData';
+import { useFetchData } from './fetchData';
 
 const Projects = () => {
-  return <div>Projects</div>;
+  const { isLoading, data } = useFetchData();
+
+  if (isLoading) {
+    return (
+      <section className="projects">
+        <h2>loading ...</h2>
+      </section>
+    );
+  }
+
+  return (
+    <section className="projects">
+      <div className="title">
+        <h2>projects</h2>
+        <div className="title-underline"></div>
+      </div>
+      <div className="projects-center">
+        {data.map((item) => {
+          const { id, title, description, img } = item;
+          return (
+            <a
+              key={id}
+              href={description}
+              target="_blank"
+              rel="noreferrer"
+              className="project"
+            >
+              <img src={img} alt={title} className="img" />
+              <h5>{title}</h5>
+            </a>
+          );
+        })}
+      </div>
+    </section>
+  );
 };
 export default Projects;
--- src/fetchData.jsx
+++ src/fetchData.jsx
@@ -1,4 +1,5 @@
 import { createClient } from 'contentful';
+import { useState, useEffect } from 'react';
 
 const client = createClient({
   space: '8hjeisht16s3',
@@ -6,6 +7,32 @@ const client = createClient({
   accessToken: 'UoNmUGx_r4Dz8X9uWXom6USeMRnpKw6vCN8-ckQ44D4',
 });
 
-client.getEntries({ content_type: 'projects' }).then((response) => {
-  console.log(response);
-});
+export const useFetchData = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [data, setData] = useState({});
+
+  const getData = async () => {
+    try {
+      const response = await client.getEntries({ content_type: 'projects' });
+      const data = response.items.map((item) => {
+        const id = item.sys.id;
+        const { title, description, image } = item.fields;
+        const img = image?.fields?.file?.url;
+
+        return { id, title, description, img };
+      });
+
+      setData(data);
+      setIsLoading(false);
+    } catch (error) {
+      console.log(error);
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  return { isLoading, data };
+};
